c8c54b75c0b5ae3bcc36b269b17d1261ab6174e0
aftab-hub <aftabaftab1158@gmail.com>
2025-07-06 14:52:30 +0530

    updating code

---
+++ src/assets/assests.js
@@ -0,0 +1,169 @@
+import React from "react";
+
+
+export const product = [
+  {
+    id: 1,
+    name: "Apple iPhone 15 (128 GB) - Black",
+    price : "₹58,999",
+    mrp : "M.R.P.: ₹69,900",
+    specifications: [
+    
+    ],
+    description: "Sleek Design Meets Powerful Performance.",
+    image: "https://m.media-amazon.com/images/I/71657TiFeHL._SL1500_.jpg",
+    link : "https://amzn.to/4eE5PhB"
+  },
+
+  {
+    id: 2,
+    name: "iPhone 16e 128 GB: Built for Apple Intelligence, A18 Chip, Supersized Battery Life, 48MP Fusion. Camera, 15.40 cm (6.1″) Super Retina XDR Display; Black",
+    price : "₹53,600",
+    mrp : "M.R.P.: ₹59,900",
+    specifications: [
+    
+    ],
+    description: "Sleek Design Meets Powerful Performance.",
+    image: "https://m.media-amazon.com/images/I/71657TiFeHL._SL1500_.jpg",
+    link : "https://amzn.to/4l8ga7O"
+  },
+  {
+    id: 3,
+    name: "Honeywell Moxie V1200 Bluetooth TWS Earbuds with 26H of Playtime, Type C Fast Charging, IPx4 Water Resistant, Bluetooth V5.3, 13mm*2 Drivers, Voice Assistant, 2 Years Manufacturer Warranty - Black",
+    price : " ₹899",
+    mrp : "M.R.P.: ₹4,299",
+    description: "Seamless connectivity with Bluetooth V5.3 and 10m of Bluetooth range.",
+    image: "https://m.media-amazon.com/images/I/51q3-468otL._SX679_.jpg",
+    link : "https://amzn.to/4krDSuC"
+  },
+  {
+    id: 4,
+    name: "LG 1.5 Ton 3 Star DUAL Inverter Split AC (Copper, AI Convertible 6-in-1, VIRAAT Mode, Diet Mode+, Faster Cooling & Energy Saving, HD Filter with Anti-Virus Protection, US-Q18JNXE, White)",
+    price : "₹36,490",
+    mrp : "M.R.P.: ₹78,990",
+    description: "Offers fast cooling, energy efficiency, and quiet performance. ",
+    image: "https://m.media-amazon.com/images/I/71zw-S62mlL._SL1500_.jpg",
+    link : "https://amzn.to/3Icw02K"
+  },
+  
+
+  
+];
+
+export const productDetails = {
+  1: { 
+    image: [
+      "https://m.media-amazon.com/images/I/71657TiFeHL._SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/51brdXeugJL._SX679_.jpg",
+      "https://m.media-amazon.com/images/I/712CBkmhLhL._SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/61f4dTush1L._SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/81BnjSLm2oL._SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/617JW0DrG8L._SL1500_.jpg"
+    ], 
+    name: "Apple iPhone 15 (128 GB) - Black",
+    title : "About this item",
+    description : [
+       "DYNAMIC ISLAND COMES TO IPHONE 15 — Dynamic Island bubbles up alerts and Live Activities — so you don’t miss them while you’re doing something else. You can see who’s calling, track your next ride, check your flight status, and so much more.",
+      "INNOVATIVE DESIGN — iPhone 15 features a durable color-infused glass and aluminum design. It’s splash, water, and dust resistant. The Ceramic Shield front is tougher than any smartphone glass. And the 6.1 Super Retina XDR display is up to 2x brighter in the sun compared to iPhone 14.",
+      "48MP MAIN CAMERA WITH 2X TELEPHOTO — The 48MP Main camera shoots in super-high resolution. So it’s easier than ever to take standout photos with amazing detail. The 2x optical-quality Telephoto lets you frame the perfect close-up.",
+       "NEXT-GENERATION PORTRAITS — Capture portraits with dramatically more detail and color. Just tap to shift the focus between subjects — even after you take the shot.",
+       "POWERHOUSE A16 BIONIC CHIP — The superfast chip powers advanced features like computational photography, fluid Dynamic Island transitions, and Voice Isolation for phone calls. And A16 Bionic is incredibly efficient to help deliver great all-day battery life."
+],
+
+    affiliateLink : "https://amzn.to/4eE5PhB"
+   
+  },
+  2: { 
+    image: [
+      "https://m.media-amazon.com/images/I/71657TiFeHL._SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/51brdXeugJL._SX679_.jpg",
+      "https://m.media-amazon.com/images/I/712CBkmhLhL._SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/61f4dTush1L._SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/81BnjSLm2oL._SL1500_.jpg",
+      "https://m.media-amazon.com/images/I/617JW0DrG8L._SL1500_.jpg"
+    ], 
+    name: "Apple iPhone 16 (128 GB) - Black",
+    title : "About this item",
+    description: [
+      "BUILT FOR APPLE INTELLIGENCE — Personal, private, powerful. Write, express yourself and get things done effortlessly",
+       "A18 CHIP. FAST INTO THE FUTURE — A18 chip empowers Apple Intelligence, gaming and iOS updates for years to come.",
+       "SUPERSIZED BATTERY LIFE — Text, browse and binge movies and shows with up to 26 hours of video playback — the best battery life in a 6.1″ iPhone.",
+       "CAMERAS — The 2-in-1 camera system has a 48MP Fusion camera for super-high-resolution photos and a 2x optical-quality Telephoto. Take incredible selfies with the 12MP front camera.",
+       "DURABLE DESIGN. BRILLIANT DISPLAY — Stunning 15.40 cm (6.1″) Super Retina XDR display. Ceramic Shield front, tougher than any smartphone glass.",
+       "ACTION BUTTON — Get quick, customisable access to your favourite apps and features, or use visual intelligence to learn about your surroundings.",
+       "MAKE IT YOURS — With iOS 18, you can customise your Home Screen and Lock Screen, send messages with text effects, tap back with any emoji and more.",
+       "PRIVACY. THAT’S IPHONE — Use Face ID to securely unlock, sign in and pay. With Private Cloud Compute, no one but you can access your data — not even Apple."
+    ], 
+
+    affiliateLink : "https://amzn.to/4l8ga7O"
+   
+  },
+  
+  3: { 
+    name: "Honeywell Moxie V1200 Bluetooth TWS Earbuds with 26H of Playtime, Type C Fast Charging, IPx4 Water Resistant, Bluetooth V5.3, 13mm*2 Drivers, Voice Assistant, 2 Years Manufacturer Warranty - Black", 
+    image: [
+        "https://m.media-amazon.com/images/I/61ywQmMEltL._SL1500_.jpg",
+        "https://m.media-amazon.com/images/I/81ZHEOtj43L._SL1500_.jpg",
+        "https://m.media-amazon.com/images/I/71wEL4D0UzL._SL1500_.jpg",
+        "https://m.media-amazon.com/images/I/71zw-S62mlL._SL1500_.jpg",
+        "https://m.media-amazon.com/images/I/71FcLYhaQoL._SL1500_.jpg",
+        "https://m.media-amazon.com/images/I/716igtUfJWL._SL1500_.jpg",
+        "https://m.media-amazon.com/images/I/81DLlHVZPoL._SL1500_.jpg",
+        "https://m.media-amazon.com/images/I/71M17vsAziL._SL1500_.jpg",
+        "https://m.media-amazon.com/images/I/71pw5OIfY1L._SL1500_.jpg",
+        "https://m.media-amazon.com/images/I/81KPV4JqkLL._SL1500_.jpg",
+        "https://m.media-amazon.com/images/I/81qZVnLWSpL._SL1500_.jpg",
+        "https://m.media-amazon.com/images/I/81Nq7B6iRQL._SL1500_.jpg",
+        "https://m.media-amazon.com/images/I/715nwMWnxKL._SL1500_.jpg",
+        "https://m.media-amazon.com/images/I/81PEI9E+yOL._SL1500_.jpg"
+    ], 
+    title : "About this item",
+    description: [
+      "Capacity: 1.5 Ton Suitable for Small Sized Rooms (111 to 150 sq.ft.); Air Circulation: 441/1080 (In/Out) CFM, Cooling Capacity 4400 W & Ambient Temperature: 55 degree Celsius with 2 way air swing",
+      "Energy Rating: 3 Star - Energy efficiency | Annual Energy Consumption: 852.44 units. ISEER Value: 4.0 (Please Refer Energy Label on Product Page or Contact Brand for More Details)",
+      "Manufacturer Warranty: 1 Year Comprehensive on Product, 5 Year on PCB and Motor & 10 Year on Compressor (*Free Gas charging during comprehensive period only | T&C Apply)",
+      "100% Copper Tubes with Ocean Black Protection: Increases Durability; Uninterrupted Cooling; Prevents Rust & Corrosion",
+      "Key Features: Dual Inverter Compressor; AI Convertible 6-in-1; 2 Way Air Swing; VIRAAT Mode; HD Filter with Anti-Virus Protection; ADC Sensor; Cools at 55⁰ C; Stabilizer free operation within 120V-290V voltage range , Temperature Display type – Magic Display (LED) for temperature and timer modes, Noise Level: IDU - 26 (db) & ODU – 55 (db)",
+      "Special Features: Gold Fin+ - Increase durability, Ocean black protection, Ez Clean Filter; Low Gas Detection; 100% Copper Condenser; Hi Grooved Copper; Smart Diagnosis System; Comfort Air; Monsoon Comfort; Self Diagnosis; Auto Clean; On/Off Timer; Sleep Mode; Diet Mode Plus; Fan Speed Steps: 6; Auto Restart (Memory); On/Off Indicator; Dehumidifier",
+      "Refrigerant gas: R32 - Environment-Friendly - Zero Ozone Depletion (ODP) & Lower Global Warming Potential (GWP)",
+      "Item Dimensions & Weight: Indoor Unit dimension IDU (W x H x D cms) (83.7 x 30.8 x 18.9); Outdoor Unit dimension ODU (W x H x D cms) (71.7 x 49.5 x 23) & IDU Weight 8.0 Kg and ODU Weight 24.5 Kg",
+      "Included In The Box: 1 Indoor Unit, 1 Outdoor Unit, 3 M (9.84 Feet) Length Inter Connecting Copper Pipe, 1 Remote, 1 Manual, 1 Warranty Card"
+    ], 
+    affiliateLink: "https://amzn.to/4krDSuC", 
+  },
+  
+  4: { 
+    name: "LG 1.5 Ton 3 Star DUAL Inverter Split AC (Copper, AI Convertible 6-in-1, VIRAAT Mode, Diet Mode+, Faster Cooling & Energy Saving, HD Filter with Anti-Virus Protection, US-Q18JNXE, White)", 
+    image: [
+        "https://m.media-amazon.com/images/I/31m-ySetxML._SX38_SY50_CR,0,0,38,50_.jpg",
+        "https://m.media-amazon.com/images/I/71VYPBVfm2L._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/71tA1mdHgKL._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/81HHPppKxeL._SX679_.jpg",
+        "https://m.media-amazon.com/images/I/71eyFoHouyL._SL1500_.jpg",
+        "https://m.media-amazon.com/images/I/81T2W-mjHaL._SL1500_.jpg",
+        "https://m.media-amazon.com/images/I/816vYKmXTpL._SL1500_.jpg",
+        "https://m.media-amazon.com/images/I/71cWKIAkOaL._SL1500_.jpg",
+        "https://m.media-amazon.com/images/I/71UcQ5903NL._SL1500_.jpg",
+        "https://m.media-amazon.com/images/I/71UO9nuwJ3L._SL1500_.jpg",
+        "https://m.media-amazon.com/images/I/713+Tjb+diL._SL1500_.jpg",
+    ], 
+    title : "About this item",
+    description: [
+      "LG Split AC with Dual inverter compressor: Experience powerful cooling with LG Split AC, featuring an energy-efficient inverter compressor for optimal performance and long-lasting durability.",
+       "Capacity: 1.5 Ton Suitable for Small Sized Rooms (111 to 150 sq.ft.); Air Circulation: 441/1080 (In/Out) CFM, Cooling Capacity 4400 W & Ambient Temperature: 55 degree Celsius with 2 way air swing",
+      "Energy Rating: 3 Star - Energy efficiency | Annual Energy Consumption: 852.44 units. ISEER Value: 4.0 (Please Refer Energy Label on Product Page or Contact Brand for More Details)",
+      "Manufacturer Warranty: 1 Year Comprehensive on Product, 5 Year on PCB and Motor & 10 Year on Compressor (*Free Gas charging during comprehensive period only | T&C Apply)",
+      "100% Copper Tubes with Ocean Black Protection: Increases Durability; Uninterrupted Cooling; Prevents Rust & Corrosion",
+      "Key Features: Dual Inverter Compressor; AI Convertible 6-in-1; 2 Way Air Swing; VIRAAT Mode; HD Filter with Anti-Virus Protection; ADC Sensor; Cools at 55⁰ C; Stabilizer free operation within 120V-290V voltage range , Temperature Display type – Magic Display (LED) for temperature and timer modes, Noise Level: IDU - 26 (db) & ODU – 55 (db)",
+      "Special Features: Gold Fin+ - Increase durability, Ocean black protection, Ez Clean Filter; Low Gas Detection; 100% Copper Condenser; Hi Grooved Copper; Smart Diagnosis System; Comfort Air; Monsoon Comfort; Self Diagnosis; Auto Clean; On/Off Timer; Sleep Mode; Diet Mode Plus; Fan Speed Steps: 6; Auto Restart (Memory); On/Off Indicator; Dehumidifier",
+      "Refrigerant gas: R32 - Environment-Friendly - Zero Ozone Depletion (ODP) & Lower Global Warming Potential (GWP)",
+      "Item Dimensions & Weight: Indoor Unit dimension IDU (W x H x D cms) (83.7 x 30.8 x 18.9); Outdoor Unit dimension ODU (W x H x D cms) (71.7 x 49.5 x 23) & IDU Weight 8.0 Kg and ODU Weight 24.5 Kg",
+      "Included In The Box: 1 Indoor Unit, 1 Outdoor Unit, 3 M (9.84 Feet) Length Inter Connecting Copper Pipe, 1 Remote, 1 Manual, 1 Warranty Card"
+    ], 
+    affiliateLink: "https://amzn.to/3Icw02K", 
+  },
+
+  
+}
+
+
